ba1aa1773bdd09c5c707016f6a5ff0529fe4e90f
sarojkumar2003 <sarojkumarsharma2003@gmail.com>
2024-07-05 15:19:10 +0530

    Review with js

---
+++ script.js
@@ -0,0 +1,13 @@
+const person1 = {
+  Name: "Susan Sumit",
+  Role: "Web Development",
+  Message:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ullam quos molestiae perspiciatis. Explicabo, et. Facilis maxime, nostrum exercitationem doloribus totam modi adipisci delectus illum numquam quibusdam, tempore repellat dolorum.",
+};
+
+// For section 1
+document.querySelector(".name").innerHTML = person1.Name;
+document.querySelector(".sub-hed").innerHTML = person1.Role;
+document.querySelector(".paragrpah").innerHTML = person1.Message;
+
+
